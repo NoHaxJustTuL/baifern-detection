d7d9b7014fe6017bc91e962bf3f4ca22b4de6192
NoHaxJustTuL <thanachot.tul@gmail.com>
2026-05-29 01:17:37 +0700

    Migrate API code into main repository

---
+++ baifern-detection-api/README.md
@@ -0,0 +1,15 @@
+---
+title: Baifern Detection Api
+emoji: 😻
+colorFrom: pink
+colorTo: blue
+sdk: gradio
+sdk_version: 6.15.1
+python_version: '3.13'
+app_file: app.py
+pinned: false
+license: apache-2.0
+short_description: backend to host api for Baifern Detection
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
---
+++ baifern-detection-api/app.py
@@ -0,0 +1,26 @@
+import gradio as gr
+from fastai.vision.all import *
+
+# 1. Load model
+learn = load_learner('model.pkl')
+
+# Extract labels (['Baifern', 'NotBaifern'])
+categories = learn.dls.vocab
+
+# 2. Define the prediction function
+def get_confidence_levels(img):
+    # predict() returns the prediction, the index, and the raw probabilities.
+    _, _, probs = learn.predict(img)
+    
+    # Create a dictionary mapping the categories to their confidence levels
+    # Result looks like: {"Baifern": 0.98, "NotBaifern": 0.02}
+    return dict(zip(categories, map(float, probs)))
+
+# 3. Create the API interface
+intf = gr.Interface(
+    fn=get_confidence_levels, 
+    inputs=gr.Image(type="pil"), # Expects an image, converts it to PIL format
+    outputs=gr.JSON()            # Outputs a raw JSON dictionary
+)
+
+intf.launch()
